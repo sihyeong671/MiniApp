b1faaca74effc46ea38e2125dd2f5af0dc9b74ef
sihyeong671 <bshlab671@naver.com>
2021-12-29 21:32:20 +0900

    make bottomnavigationbar

---
+++ app/src/main/java/com/example/miniapp/ContactsFragment.kt
@@ -0,0 +1,48 @@
+package com.example.miniapp
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ContactsFragment : Fragment(){
+
+
+    companion object{
+        const val TAG : String = "로그"
+
+        fun newInstance(): ContactsFragment{
+            return ContactsFragment()
+        }
+    }
+
+    // 메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "ContactsFragment - onCreate called")
+
+    }
+
+
+    // 프레그먼트를 안고 있는 액티비티에 붙었을 때
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "ContactsFragment - onAttach() called")
+    }
+
+
+    //뷰 생성
+    // fragment와 레이아웃 연결
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "ContactsFragment - onCreateView() called")
+
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+}
